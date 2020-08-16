998cf47e56b6d0ac9073defe119abdf32a81ca8c
Sultan0099 <syedsultan0099@gmail.com>
2020-08-17 02:53:15 +0500

    resolve uploads folder error

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,10 +1,16 @@
 import multer from "multer";
 
 import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
+
         const uploadDir = path.join(__dirname, "..", "..", "uploads");
+
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+        }
 
         cb(null, uploadDir);
     },
